cbf9f0f08e543b2815fc676b96dfb7dcd6635a75
Pujavn <naikpujavinayak@gmail.com>
2017-06-29 01:56:38 +0530

    [TASK] Check if pushing to correct repository

--- Classes/Cloudframe/Leavemanagement/Controller/EmployeeController.php
+++ Classes/Cloudframe/Leavemanagement/Controller/EmployeeController.php
@@ -81,6 +81,7 @@ class EmployeeController extends ActionController
     {
         $this->employeeRepository->remove($employee);
         $this->addFlashMessage('Deleted a employee.');
+        echo "hi"exit;
         $this->redirect('index');
     }
 
